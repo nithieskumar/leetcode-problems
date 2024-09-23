4c8d5c4e24a43604c73536fb68ccd148bc93bd75
nithieskumar <125538932+nithieskumar@users.noreply.github.com>
2024-09-23 11:34:26 +0530

    Time: 1 ms (62.05%), Space: 44.4 MB (13.82%) - LeetHub

--- 3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.java
+++ 3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.java
@@ -3,8 +3,6 @@ class Solution {
         int n=nums.length;
         int sum1=0;
         int sum2=0;
-        int sum3=0;
-        int sum4=0;
         for(int i=0;i<n;i++)
         {
             if(nums[i]<10)
@@ -15,20 +13,10 @@ class Solution {
                 sum2+=nums[i];
             }
         }
-        for(int i=0;i<n;i++)
+        if(sum1==sum2)
         {
-            if(nums[i]>9)
-            {
-                sum3+=nums[i];
-            }
-            else{
-                sum4+=nums[i];
-            }
+            return false;
         }
-        if(sum1>sum2||sum3>sum4)
-        {
-            return true;
-        }
-        return false;
+        return true;
     }
 }
