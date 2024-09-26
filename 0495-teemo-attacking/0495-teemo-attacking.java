12b8d0d3bf8c933898d4523763456547b136ba43
nithieskumar <125538932+nithieskumar@users.noreply.github.com>
2024-09-26 13:47:58 +0530

    Time: 3 ms (82.38%), Space: 44.6 MB (94.97%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.java
+++ 0495-teemo-attacking/0495-teemo-attacking.java
@@ -4,7 +4,7 @@ class Solution {
         int k=0;
         for(int i=0;i<n-1;i++)
         {
-            if(t[i+1]<=t[i]+d-1)
+            if(t[i+1]<=t[i]+d)
             {
                 k+=t[i+1]-t[i];
             }
