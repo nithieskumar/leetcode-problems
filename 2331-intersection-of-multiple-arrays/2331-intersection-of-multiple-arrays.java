3abeb9b88d7a39ff9d0c80a4ecdc10390c157538
nithieskumar <125538932+nithieskumar@users.noreply.github.com>
2024-09-26 10:06:01 +0530

    Time: 2 ms (100%), Space: 44.9 MB (30.69%) - LeetHub

--- 2331-intersection-of-multiple-arrays/2331-intersection-of-multiple-arrays.java
+++ 2331-intersection-of-multiple-arrays/2331-intersection-of-multiple-arrays.java
@@ -1,31 +1,15 @@
 class Solution {
     public List<Integer> intersection(int[][] nums) {
         int c[]=new int[1001];
-         for(int j=0;j<nums[0].length;j++)
-         {
-                c[nums[0][j]]++;
-         }
-         for(int i=1;i<nums.length;i++)
-         {
-            boolean []a=new boolean[1001];
-            for(int j=0;j<nums[i].length;j++)
-            {
-                if(c[nums[i][j]]==i)
-                {
-                    a[nums[i][j]]=true;
-                }
-            }
-         
-         for(int k=0;k<1001;k++)
-         {
-            if(a[k])
-            {
-                c[k]++;
-            }
-         }
-         }
          List<Integer> l=new ArrayList<>();
-         for(int i=0;i<1001;i++)
+         for(int[]a:nums)
+         {
+            for(int i:a)
+            {
+                c[i]++;
+            }
+         }
+         for(int i=0;i<c.length;i++)
          {
             if(c[i]==nums.length)
             {
