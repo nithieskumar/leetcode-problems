43c0abe03f9fae654859ed785da8ee20754af57c
nithieskumar <125538932+nithieskumar@users.noreply.github.com>
2024-09-26 13:46:02 +0530

    Time: 3 ms (82.38%), Space: 44.9 MB (62.85%) - LeetHub

---
+++ 0495-teemo-attacking/0495-teemo-attacking.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findPoisonedDuration(int[] t, int d){
+        int n=t.length;
+        int k=0;
+        for(int i=0;i<n-1;i++)
+        {
+            if(t[i+1]<=t[i]+d-1)
+            {
+                k+=t[i+1]-t[i];
+            }
+            else{
+                k+=d;
+            }
+        }
+        k+=d;
+        return k;
+    }
+}
